82dd248d678fe1ddca30f4831b6a079ecb7450d6
Md Imran Parves <89092460+imran21-dev@users.noreply.github.com>
2024-07-28 23:03:04 +0600

    onek kisu baki

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkAll() {
+      const div = document.querySelector(".project-cart-img")
+            
+      const img = document.getElementById("java-img")
+      const popimg = document.getElementById("java-pop-img")
+  
+}
